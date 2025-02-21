16aea3e1a9559e681e685ba8f7b0fb3376372306
Tolu1996 <kentorooyekanmi@gmail.com>
2025-02-21 10:32:29 -0700

    Create feb20 2025 DAY3.py

---
+++ feb20 2025 DAY3.py
@@ -0,0 +1,7 @@
+#I learned that when tying a variable to input and questions, you can make so many different questions and surveys.
+myFood = input("Name a food > ")
+myPlant = input("Name a type of plant > ")
+myCookmethod = input("Name a method of cooking > ")
+myHouseholditem = input("Name a household item > ")
+myBurnedfood = input("What word describes burned food? > ")
+print (myCookmethod, myFood, "with", myBurnedfood, myPlant,)
